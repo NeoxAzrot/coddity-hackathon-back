aa20af0038dbf9fe58b7e086cb7025f2d364a824
NeoxAzrot <sami.lepays@gmail.com>
2022-04-30 22:14:40 +0200

    fix(server): try cors options

--- server.ts
+++ server.ts
@@ -1,5 +1,5 @@
 import bodyParser from 'body-parser';
-import cors from 'cors';
+import cors, { CorsOptions } from 'cors';
 import dotenv from 'dotenv';
 import dotenvExpand from 'dotenv-expand';
 import express from 'express';
@@ -10,27 +10,32 @@ import apolloServer from './graphql/connect';
 dotenvExpand.expand(dotenv.config());
 const API_PORT = process.env.PORT || 3001;
 
-// const whitelist = [
-//   'http://localhost:3000',
-//   'http://localhost:3001',
-//   'http://warmd.fr',
-//   'http://www.warmd.fr',
-//   'http://warmd-api.herokuapp.com',
-// ];
-// const corsOptions: CorsOptions = {
-//   origin: (origin, callback) => {
-//     if (whitelist.indexOf(origin as string) !== -1 || !origin) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-// };
+const whitelist = [
+  'http://localhost:3000',
+  'http://localhost:3001',
+  'http://warmd.fr',
+  'https://warmd.fr',
+  'http://www.warmd.fr',
+  'https://www.warmd.fr',
+  'http://warmd-api.herokuapp.com',
+  'https://warmd-api.herokuapp.com',
+  'http://warmd.netlify.app',
+  'https://warmd.netlify.app',
+];
+const corsOptions: CorsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin as string) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 const startServer = async () => {
   await apolloServer({ app, port: API_PORT });
